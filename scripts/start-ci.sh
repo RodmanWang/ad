790f508ceba7b10325745751af8a9ebbe167cd64
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2021-05-01 18:31:32 +0800

    Update start-ci.sh

--- scripts/start-ci.sh
+++ scripts/start-ci.sh
@@ -5,9 +5,8 @@ source /etc/profile
 cd $(cd "$(dirname "$0")";pwd)
 
 easylist=(
-  "https://easylist-downloads.adblockplus.org/easylist.txt"
-  "https://easylist-downloads.adblockplus.org/easylistchina.txt"
-  "https://easylist-downloads.adblockplus.org/easyprivacy.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt"
   "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
 )
 
