0fc76f49ab855b2e59f9f11ed405f2c98ced1155
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2022-03-20 13:28:42 +0800

    Update white_domain_list.php

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -332,6 +332,8 @@ return array(
     'gbfek.dfcfw.com' => 0, #593
     'dfscdn.dfcfw.com' => 0, #593
     'empic.dfcfw.com' => 0, #593
+    'wxapp.lvyadx.cn' => 0, #584
+    'canyoublockit.com' => 0, #601
 
 
 
