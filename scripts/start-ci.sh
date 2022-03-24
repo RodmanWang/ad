3e6fc97a56299a72dafa61f981b8d1a1d44ac951
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2022-03-25 04:29:46 +0800

    Update start-ci.sh

--- scripts/start-ci.sh
+++ scripts/start-ci.sh
@@ -5,10 +5,14 @@ source /etc/profile
 cd $(cd "$(dirname "$0")";pwd)
 
 easylist=(
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_2_English/filter.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_4_Social/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt"
-  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_15_DnsFilter/filter.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_224_Chinese/filter.txt"
   "https://secure.fanboy.co.nz/fanboy-annoyance.txt"
+  "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
 )
 
 hosts=(
