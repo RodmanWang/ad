b232f1474dadbc3cb1dfef7caa3eb3b465e053bd
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2022-04-22 23:26:39 +0800

    Update white_domain_list.php

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -1,7 +1,7 @@
 <?php
 //white_domain_list
 //白名单机制...，白名单是
-//@date 2018年12月23日
+//@date 2022年04月22日
 //value=-1,代表失效本条规则，暂只支持单域名（针对引入外部白名单时的精确控制）,当处于strict_mode时，排除此key，单条关闭strict_mode
 //value=0,代表仅加白单条域名
 //value=1,代表其下级域名全部加白（例如3级域名，则其4级子域名全部加白）
@@ -334,6 +334,11 @@ return array(
     'empic.dfcfw.com' => 0, #593
     'wxapp.lvyadx.cn' => 0, #584
     'canyoublockit.com' => 0, #601
+    'd.getpocket.com' => 0, #602
+    'pdst.fm' => 0, #603
+    'licensebuttons.net' => 0, #609
+    'wmflabs.org' => 0, #611
+    'jsinfo.net' => 0, #613
 
 
 
