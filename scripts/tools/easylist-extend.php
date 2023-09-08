190f5ddd3b608a9dc4f7ac180b44b21b51f9df04
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2023-09-08 23:20:02 +0800

    Update easylist-extend.php

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -258,6 +258,9 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||ads.95516.com^' => 1, // #695
     '@@||track.bankcomm.com^' => 1, // #714
     '@@||tongji.koowo.cn^' => 1, // #742
+    '@@||adverts.1foo.com^' => 1, // #782
+    '@@||track.4px.com^' => 1, // #796
+    '@@||ads.smartmidea.net^' => 1, // #807
 );
 
 //针对上游赦免规则anti-AD不予赦免的规则，即赦免名单的黑名单
@@ -578,6 +581,7 @@ $ARR_WHITE_RULE_BLK_LIST = array(
     '@@||zbj.com^' => null,
     '@@||zeplin.io^' => null,
     '@@||zippyshare.com^' => null,
+    '@@||ads.tiktok.com^' => null,
 );
 
 //针对上游通配符规则中anti-AD不予采信的规则，即通配符黑名单
