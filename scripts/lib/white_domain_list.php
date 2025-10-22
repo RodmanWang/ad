f20c7448ab3d647d95de7f4363ac2e4a1bf31a8d
RodmanWang <19368219+RodmanWang@users.noreply.github.com>
2025-10-23 00:41:37 +0800

    Update white_domain_list.php

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -453,6 +453,10 @@ return [
     //"zyngaplayersupport.com" => 0, // #1060
     "adukwai.com" => 0, // #1063
     "snowflake.qq.com" => 0, // #1068
+    "onelink.me" => 0, // #1074
+    "ac.dun.163.com" => 0, // #1014
+    "ac.dun.163yun.com" => 0, // #1014
+    "scarf.sh" => 0, // #957, #993, #1076
 
     // upstream-white-easylist 中选取的一些应放行域名
     "a1.itc.cn" => 0,
